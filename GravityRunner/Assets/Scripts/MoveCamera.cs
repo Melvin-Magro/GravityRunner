fb29ceb0844330c548d81f6789686db784533d3f
Melvin-Magro <34582963+Melvin-Magro@users.noreply.github.com>
2018-05-19 14:09:19 +0200

    Camera Movement & Temporary Assets

---
+++ GravityRunner/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    private float speed = 3f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //moving the camera on the x position by the amount 
+        //allocated in the speed variable in real time
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+	}
+}
